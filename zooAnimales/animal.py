870c434538eb0a3a6f78e7289528b80a0c2459db
jtobonz <92930935+jtobonz@users.noreply.github.com>
2022-09-26 00:20:46 -0500

    hkl

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -50,7 +50,7 @@ class Animal:
     
     @classmethod
     def toString(self):
-            #return f"“Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.self._zoo}"
+        if self._zona == None:
             return f"“Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
-        #else:
-            #return f"“Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.self._zoo}"
+        else:
+            return f"“Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.self._zoo}"
